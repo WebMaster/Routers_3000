9ecb13efc58459f7e34f5d78c1b498834be83007
Alexander <Alexander.Statsenko@gmail.com>
2025-11-25 09:51:21 +0300

    Create install_and_update.sh

---
+++ install_and_update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#chmod +x /tmp/install_and_update.sh && /tmp/install_and_update.sh
+
+# Добавляем задание на выполнение скрипта каждый день в 4часа 10минут
+#cronTask="*/1 * * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+cronTask="10 4 * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+#str=$(grep -i "\*\/1 \* \* \* \* sh \<\(wget -q -O - https:\/\/raw\.githubusercontent\.com\/WebMaster\/Routers_3000\/refs\/heads\/main\/install_and_update\.sh\) 2\>\&1 \| tee \/root\/run\.log" /etc/crontabs/root)
+str=$(grep -i "10 4 \* \* \* sh \<\(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
+if [ -z "$str" ] 
+then
+    echo "Add cron task auto run script install_and_update.sh"
+    echo "$cronTask" >> /etc/crontabs/root
+fi
+
+echo "Complete"
