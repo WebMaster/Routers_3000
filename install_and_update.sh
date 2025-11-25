0a441c70a4fa2723a7ed8a2296993c497bbe2b17
Alexander <Alexander.Statsenko@gmail.com>
2025-11-25 10:08:44 +0300

    init

--- install_and_update.sh
+++ install_and_update.sh
@@ -2,6 +2,48 @@
 
 #chmod +x /tmp/install_and_update.sh && /tmp/install_and_update.sh
 
+opkg update
+
+#проверяем установлени ли пакет dnsmasq-full
+if opkg list-installed | grep -q dnsmasq-full; then
+	echo "dnsmasq-full already installed..."
+else
+	echo "Installed dnsmasq-full..."
+	cd /tmp/ && opkg download dnsmasq-full
+	opkg remove dnsmasq && opkg install dnsmasq-full --cache /tmp/
+
+	[ -f /etc/config/dhcp-opkg ] && cp /etc/config/dhcp /etc/config/dhcp-old && mv /etc/config/dhcp-opkg /etc/config/dhcp
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Добавляем задание на выполнение скрипта каждый день в 4часа 10минут
 #cronTask="*/10 * * * * sh <(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
 cronTask="10 4 * * * sh <(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
