48c1b8af2a934100c1866373527c825c18275ffc
Alexander <Alexander.Statsenko@gmail.com>
2025-11-25 09:57:31 +0300

    init

--- install_and_update.sh
+++ install_and_update.sh
@@ -3,10 +3,10 @@
 #chmod +x /tmp/install_and_update.sh && /tmp/install_and_update.sh
 
 # Добавляем задание на выполнение скрипта каждый день в 4часа 10минут
-#cronTask="*/1 * * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
-cronTask="10 4 * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
-#str=$(grep -i "\*\/1 \* \* \* \* sh \<\(wget -q -O - https:\/\/raw\.githubusercontent\.com\/WebMaster\/Routers_3000\/refs\/heads\/main\/install_and_update\.sh\) 2\>\&1 \| tee \/root\/run\.log" /etc/crontabs/root)
-str=$(grep -i "10 4 \* \* \* sh \<\(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
+cronTask="*/10 * * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+#cronTask="10 4 * * * sh <(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh) 2>&1 | tee /root/run.log"
+str=$(grep -i "\*\/10 \* \* \* \* sh \<\(wget -q -O - https:\/\/raw\.githubusercontent\.com\/WebMaster\/Routers_3000\/refs\/heads\/main\/install_and_update\.sh\) 2\>\&1 \| tee \/root\/run\.log" /etc/crontabs/root)
+#str=$(grep -i "10 4 \* \* \* sh \<\(wget -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
 if [ -z "$str" ] 
 then
     echo "Add cron task auto run script install_and_update.sh"
@@ -14,3 +14,4 @@ then
 fi
 
 echo "Complete"
+wget --no-check-certificate -O /tmp/install_and_update.sh https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/install_and_update.sh && chmod +x /tmp/install_and_update.sh && /tmp/install_and_update.sh
