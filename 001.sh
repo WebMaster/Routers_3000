7c404150dbbe56c1219e1241c38d57b5900a78c4
Alexander <Alexander.Statsenko@gmail.com>
2025-11-27 13:44:20 +0300

    Update 001.sh

--- 001.sh
+++ 001.sh
@@ -3,11 +3,11 @@
 #chmod +x /tmp/001.sh && /tmp/001.sh
 NUMBER="001"
 
-cronTask="10 4 * * * sh <(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/$NUMBER.sh) 2>&1 | tee /root/run.log"
-str=$(grep -i "10 4 \* \* \* sh \<\(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/$NUMBER.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
+cronTask="10 4 * * * sh <(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/001.sh) 2>&1 | tee /root/run.log"
+str=$(grep -i "10 4 \* \* \* sh \<\(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/001.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
 if [ -z "$str" ] 
 then
-    echo "Add cron task auto run script $NUMBER.sh"
+    echo "Add cron task auto run script 001.sh"
     echo "$cronTask" >> /etc/crontabs/root
 fi
 
