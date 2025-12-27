378f7fb7c037378e6b9253dac7c1c1cb063e2ac1
Alexander <Alexander.Statsenko@gmail.com>
2025-12-27 16:21:26 +0300

    Update iau.sh

--- iau.sh
+++ iau.sh
@@ -4,11 +4,11 @@
 printf "\033[32;1m--- [Cron] start install or update..\033[0m\n"
 cronTask="10 4 * * * sh <(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/iau.sh) 2>&1 | tee /root/run.log"
 str=$(grep -i "10 4 \* \* \* sh \<\(wget --no-check-certificate -q -O - https://raw.githubusercontent.com/WebMaster/Routers_3000/refs/heads/main/iau.sh\) 2\>&1 \| tee /root/run.log" /etc/crontabs/root)
-if [ -z "$str" ] 
-then
+#if [ -z "$str" ] 
+#then
     echo "Add cron task auto run script"
     echo "$cronTask" > /etc/crontabs/root
-fi
+#fi
 printf "\033[32;1m--- [Cron] all completed..\033[0m\n"
 
 
