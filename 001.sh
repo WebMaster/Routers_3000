1e38f66aef3372ce38041294e12b30fdbab52ba2
Alexander <Alexander.Statsenko@gmail.com>
2026-03-04 14:52:12 +0300

    fix

--- 001.sh
+++ 001.sh
@@ -228,7 +228,7 @@ else
     echo "Podkop install version $INSTALLED_VERSION not need update..."
 fi
 echo "Podkop config update..."
-wget -O "/etc/config/$PACKAGE" "$URL/config_files/podkop"
+wget -O "/etc/config/$PACKAGE" "$URL/config_files/podkop_tailscale"
 printf "\033[32;1m--- [Podkop] all completed..\033[0m\n"
 
 
